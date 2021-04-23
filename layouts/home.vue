d2ba278ccadf08e2798b9db699cd26303dcdede7
KingRayhan <rayhan095@gmail.com>
2021-04-23 22:07:36 +0600

    spaceing problem on phone solved

--- layouts/home.vue
+++ layouts/home.vue
@@ -3,11 +3,11 @@
     <Navbar />
     <hero v-if="!$auth.loggedIn" />
     <div class="wrapper">
-      <div class="grid grid-cols-12 space-x-5">
+      <div class="grid grid-cols-12 md:space-x-5">
         <div class="hidden col-span-12 md:block md:col-span-3">
           <widgets-selected-tags />
         </div>
-        <div class="col-span-12 md:col-span-6 md:px-4">
+        <div class="col-span-12 md:col-span-6">
           <Nuxt />
         </div>
 
