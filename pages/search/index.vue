e270ad90997ca3dbdf86d553ce092d0770ea4625
Shoaib Sharif <shoaib19j@gmail.com>
2021-04-27 12:49:39 -0700

    search integrated

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
-    <div class="mb-4 md:hidden">
+    <div class='mb-4 pt-20 mx-2 md:pt-0 md:hidden'>
       <mobile-search />
     </div>
-    <div v-if="loading" class="my-5 text-center">
-      <SyncLoader color="#58d4e1" />
+    <div v-if='loading' class='my-5 text-center'>
+      <SyncLoader color='#58d4e1' />
     </div>
 
     <ArticleCard
       v-else
-      :isSearchCard="true"
-      v-for="article in results"
-      class="mb-5"
-      :key="article.id"
-      :article="article"
+      :isSearchCard='true'
+      v-for='article in results'
+      class='mb-5'
+      :key='article.id'
+      :article='article'
     />
   </div>
 </template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Search'
+    }
+  }
+}
+</script>
