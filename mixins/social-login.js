1088cd3bc92868f43d3831df39865262570d9ade
KingRayhan <rayhan095@gmail.com>
2021-04-30 23:10:54 +0600

    mobile menu and tag  fixed

--- mixins/social-login.js
+++ mixins/social-login.js
@@ -1,12 +1,21 @@
 export default {
   data() {
     return {
-      loading: false,
+      loadingGithub: false,
+      loadingGoogle: false,
     }
   },
   methods: {
     socialLogin(service) {
-      this.loading = true
+      // this.loading = true
+      if (service == 'google') {
+        this.loadingGoogle = true
+      }
+
+      if (service == 'github') {
+        this.loadingGithub = true
+      }
+
       window.location.href = `${process.env.NUXT_ENV_APP_URL_CLIENT}/api/auth/login/${service}`
     },
   },
