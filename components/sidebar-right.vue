69002361701a3f2b1e7dd26caf6c5eac50f81d41
KingRayhan <rayhan095@gmail.com>
2021-04-30 17:22:51 +0600

    contest banner

--- components/sidebar-right.vue
+++ components/sidebar-right.vue
@@ -1,7 +1,19 @@
 <template>
   <aside>
-    <widgets-login v-if='!$auth.loggedIn' class='mb-8' />
-    <widgets-online-users class='mb-4' v-if='$auth.loggedIn' />
+    <a
+      href="https://contest.techdiary.dev"
+      target="_blank"
+      class="inline-block mb-2"
+    >
+      <img
+        class="w-full rounded-md"
+        src="https://res.cloudinary.com/techdiary-dev/image/upload/v1619780638/static-assets/contest/qms7zyhnak1goc0aydeg.png"
+        alt="dev-article-contest-season-1"
+      />
+    </a>
+
+    <widgets-login v-if="!$auth.loggedIn" class="mb-8" />
+    <widgets-online-users class="mb-4" v-if="$auth.loggedIn" />
     <widgets-latest-registered-users />
     <sidebar-footer />
   </aside>
--- pages/_username/_articleSlug.vue
+++ pages/_username/_articleSlug.vue
@@ -39,8 +39,19 @@
             </nuxt-link>
           </div>
         </div>
+
         <!-- Title and meta end -->
         <article-user-info :article="article" />
+
+        <div class="mt-3">
+          <a href="https://contest.techdiary.dev" target="_blank">
+            <img
+              class="w-full rounded-md"
+              src="https://res.cloudinary.com/techdiary-dev/image/upload/v1619780480/static-assets/contest/k09ialie9h1cr5tir9wi.png"
+              alt="dev-article-contest-season-1"
+            />
+          </a>
+        </div>
 
         <div
           class="my-6 content-typography text-dark max-w-none"
