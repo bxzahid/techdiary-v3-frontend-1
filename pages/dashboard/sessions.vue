bf966615dff8eb0803a50150b9400a864214eaee
Md Raihan <rayhan095@gmail.com>
2021-05-04 13:17:38 +0600

    Update sessions.vue

--- pages/dashboard/sessions.vue
+++ pages/dashboard/sessions.vue
@@ -11,7 +11,7 @@
           </svg>
 
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' stroke-width='2' stroke='currentColor' fill='none'
-               stroke-linecap='round' stroke-linejoin='round' class='w-8 h-8 text-gray-500' v-else>
+               stroke-linecap='round' stroke-linejoin='round' class='w-8 h-8 text-text' v-else>
             <path d='M0 0h24v24H0z' stroke='none'></path>
             <rect x='7' y='4' width='10' height='16' rx='1'></rect>
             <path d='M11 5h2M12 17v.01'></path>
